6e0d06d33249aa6a742134eb33f0a66261a8f55c
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 17:00:10 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -577,7 +577,7 @@ useEffect(() => {
       {/* Portfolio Section */}
       <div 
   ref={portfolioSectionRef} 
-  className="relative min-h-screen w-full bg-white z-40 rounded-t-[3rem] rounded-b-[3rem] texture-overlay opacity-100 translate-y- 10"
+  className="relative min-h-screen w-full bg-white z-60 rounded-t-[3rem] rounded-b-[3rem] texture-overlay opacity-100 translate-y- 10"
 >
 
         

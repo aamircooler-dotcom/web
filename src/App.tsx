788dc033a2f5353c7d5efc95b76e1cc21d6aa687
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 13:45:54 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -336,8 +336,8 @@ function App() {
   ease: "power3.out",
   scrollTrigger: {
     trigger: portfolioSectionRef.current,
-    start: "top 50%", // trigger when bottom of element hits 80% of viewport
-    end: "bottom 60%", // optional: end when top of element hits 20% of viewport
+    start: "top 0%", // trigger when bottom of element hits 80% of viewport
+    end: "bottom 0%", // optional: end when top of element hits 20% of viewport
     toggleActions: "play reverse play reverse",
     // markers: true, // enable for debug
   }

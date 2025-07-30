07708ca6b4e191c4c532e42a3a0dbb5d3a41a91d
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 14:54:32 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -315,7 +315,7 @@ useEffect(() => {
     trigger: heroRef.current,
     start: "top top",
     end: "top+=1500", // 🔁 reduce to make it slower & smoother
-    scrub: 0.1   // 🔁 increase for smoother animation
+    scrub: 10   // 🔁 increase for smoother animation
   }
 });
    

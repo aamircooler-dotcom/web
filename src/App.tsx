1bb46e0250321d69275495d18b61b2ecc64eefbf
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 15:01:04 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -253,6 +253,8 @@ function App() {
   // Mouse tracking state
 const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
 const [isCursorInsideHero, setIsCursorInsideHero] = React.useState(false);
+  const [isMouseTrackingEnabled, setIsMouseTrackingEnabled] = useState(true);
+
 
    useEffect(() => {
     const handleMouseEnter = () => setIsCursorInsideHero(true);

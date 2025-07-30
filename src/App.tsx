2c03e33d96c1c2a315c7a066bb300c1d4a915a17
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 13:08:47 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -644,7 +644,7 @@ ScrollTrigger.create({
           </h2>
 
           {/* Subheading */}
-          <p className="text-black/60 text-lg md:text-xl mb-10 text-center font-bosenAlt">
+          <p className="text-black/30 text-lg md:text-xl mb-10 text-center font-bosenAlt">
             Drop me a message, let's make something users will love.
           </p>
 

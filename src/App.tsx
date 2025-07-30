416f39f10a24e39b2b987fdc7792d1b0cc5d8cb1
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 12:31:08 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -374,7 +374,7 @@ ScrollTrigger.create({
     ScrollTrigger.create({
       trigger: portfolioSectionRef.current,
       start: "bottom 90%",
-      end: "bottom 100%",
+      end: "bottom bottom",
       onEnter: () => setShowContact(true),
       onLeave: () => setShowContact(false),
       onEnterBack: () => setShowContact(true),

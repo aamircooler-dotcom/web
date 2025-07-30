1eac95bd9db6f4b5e402dd9087925055bc2b6b87
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 13:28:29 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -342,7 +342,7 @@ ScrollTrigger.create({
 
   // Portfolio up animation
   gsap.to(portfolioSectionRef.current, {
-    y: 0,
+    y: 200,
     opacity: 100,
     scrollTrigger: {
       trigger: heroRef.current,

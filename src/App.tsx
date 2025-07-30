f24ef4330adf456e4e4ae0f619c13cb71d8c4898
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 12:57:59 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -573,7 +573,7 @@ ScrollTrigger.create({
       {/* Portfolio Section */}
       <div 
   ref={portfolioSectionRef} 
-  className="relative min-h-screen w-full bg-white z-40 rounded-t-[3rem] rounded-b-[3rem] texture-overlay opacity-0 translate-y-10"
+  className="relative min-h-screen w-full bg-white z-40 rounded-t-[3rem] rounded-b-[3rem] texture-overlay opacity-100 translate-y-10"
 >
 
         

0a70dc0b21ab479307edaee87ca83d0ad85fec8b
aamircooler-dotcom <aamircooler@gmail.com>
2025-07-30 14:18:16 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -453,6 +453,7 @@ const [isCursorInsideHero, setIsCursorInsideHero] = React.useState(false);
   style={{
   top: '21%',
   left: '1%',
+      transition: 'transform 0.4s ease', // ✅ smooth reset
   transform: isCursorInsideHero
     ? `translate(${mousePosition.x * 8}px, ${mousePosition.y * 8}px)`
     : 'translate(0, 0)' // reset to center when outside
